107fa2bc89e7bb538909bc1e6f26f3007f128bbe
Grriffis <gjh9450@g.rit.edu>
2021-11-19 16:37:21 -0500

    fixes

--- nfl/nflpa.php
+++ nfl/nflpa.php
@@ -1,44 +1,17 @@
-
-
 <?php include "inc/nav.php";?>
 
+<body>
 
+    <article>
 
-			<article>
+        <h2> National Football League Players Association (NFLPA) </h2>
 
+        <p>The National Football League Players Association (NFLPA) is the union for professional football players in the National Football League. The union allows players to stand together to protect their health, rights, and families. The union was formed in 1956; players used their combined strength to force owners to provide basics like clean uniforms and safer equipment. Their job is to represent players’ interests during negotiations for the next CBA and to make sure players are well informed and prepared for them. In 2011, they negotiated a landmark Collective Bargaining Agreement (CBA) after an 18-week lockout by the owners. In March 2020, they negotiated the current CBA, which runs through the 2030 season. The current CBA brought new gains to the members, including an increased share in revenue, expanded roster sizes, a sizable bump in minimum salaries, health & safety advances, rookie contract bonuses, and more. The NFLPA represents all players in matters concerning wages, hours and working conditions and protects their rights as professional football players, makes sure that the terms of the Collective Bargaining Agreement (CBA) are met and negotiates and monitors retirement and insurance benefits. The union also provides other member services and activities, provides assistance to charitable and community organizations and enhances and defends the image of players and their profession on and off the field. The NFLPA works from the bottom-up. All power and authority to do anything in the NFLPA comes from the Board of Reps who are elected by the player members. Player Members on each NFL team elect both a Player Representative and an Alternate Player Representative to serve on the Board of Player Representatives (The Board of Player Reps). The Board of Player Reps meets at least once a year and makes all of the important decisions for the organization.</p>
 
-				<h2> National Football League Players Association (NFLPA) </h2>
+    </article>
 
-				<p>
-					The National Football League Players Association (NFLPA) is the union for professional football players in the National Football League. The union allows players to stand together to protect their health, rights, and families. The union was formed in 1956; players used their combined strength to force owners to provide basics like clean uniforms and safer equipment. Their job is to represent players’ interests during negotiations for the next CBA and to make sure players are well informed and prepared for them. In 2011, they negotiated a landmark Collective Bargaining Agreement (CBA) after an 18-week lockout by the owners. In March 2020, they negotiated the current CBA, which runs through the 2030 season. The current CBA brought new gains to the members, including an increased share in revenue, expanded roster sizes, a sizable bump in minimum salaries, health & safety advances, rookie contract bonuses, and more. 
+    <?php include "inc/reference.php";?>
 
-					The NFLPA represents all players in matters concerning wages, hours and working conditions and protects their rights as professional football players, makes sure that the terms of the Collective Bargaining Agreement (CBA) are met and negotiates and monitors retirement and insurance benefits. The union also provides other member services and activities, provides assistance to charitable and community organizations and enhances and defends the image of players and their profession on and off the field. The NFLPA works from the bottom-up. All power and authority to do anything in the NFLPA comes from the Board of Reps who are elected by the player members. Player Members on each NFL team elect both a Player Representative and an Alternate Player Representative to serve on the Board of Player Representatives (The Board of Player Reps). The Board of Player Reps meets at least once a year and makes all of the important decisions for the organization.
-				</p>
+</body>
 
-			</article>
-
-
-
-			<?php include "inc/reference.php";?>
-
-
-		</div>
-
-	</body>	
-
-</html>	
-
-
-
-
-
-
-
-
-
-
-
-
-
-	
-
+</html>
--- nfl/teams.php
+++ nfl/teams.php
@@ -1,38 +1,35 @@
 <?php include "inc/nav.php";?>
 
-<section class="container">
-				<article class = "teamimg">
-					
-					<img src="Untitled-2.png" alt="Teams" class = "teamimg1"> 
+<body>
 
-				</article>
+    <section class="container">
 
-				<aside class = "aside">
-					
-					<h2 class = "teams"> Teams and Divisions </h2>
+        <figure class="teamimg">
 
-					<ul class = "listys">
-						<li> <strong> AFC East </strong> Buffalo Bills, Miami Dolphins, New England Patriots, New York Jets</li>
-						<li> <strong>AFC South </strong>Houston Texans, Indianapolis Colts, Jacksonville Jaguars, Tennessee Titans</li>
-						<li><strong>AFC West</strong> Denver Broncos, Kansas City Chiefs, Oakland Raiders, San Diego Chargers</li>
-						<li><strong>NFC North </strong>Chicago Bears, Detroit Lions, Green Bay Packers, Minnesota Vikings</li>
-						<li><strong>NFC East </strong>Dallas Cowboys, New York Giants, Philadelphia Eagles, Washington Redskins</li>
-						<li><strong>NFC South </strong>Atlanta Falcons, Carolina Panthers, New Orleans Saints, Tampa Bay Buccaneers</li>
-						<li><strong>NFC West </strong>Arizona Cardinals, Los Angeles Rams, San Francisco 49ers, Seattle Seahawks</li>
-					</ul>
+            <img src="images/Untitled-2.png" alt="Teams" class="teamimg1">
 
+        </figure>
 
+        <aside class="aside">
 
-	  
+            <h2 class="teams"> Teams and Divisions </h2>
 
-				</aside>
-				</section>
-		
-			
+            <ul class="listys">
+                <li><strong>AFC East: </strong> Buffalo Bills, Miami Dolphins, New England Patriots, New York Jets</li>
+                <li><strong>AFC South: </strong>Houston Texans, Indianapolis Colts, Jacksonville Jaguars, Tennessee Titans</li>
+                <li><strong>AFC West: </strong> Denver Broncos, Kansas City Chiefs, Oakland Raiders, San Diego Chargers</li>
+                <li><strong>NFC North: </strong>Chicago Bears, Detroit Lions, Green Bay Packers, Minnesota Vikings</li>
+                <li><strong>NFC East: </strong>Dallas Cowboys, New York Giants, Philadelphia Eagles, Washington Redskins</li>
+                <li><strong>NFC South: </strong>Atlanta Falcons, Carolina Panthers, New Orleans Saints, Tampa Bay Buccaneers</li>
+                <li><strong>NFC West: </strong>Arizona Cardinals, Los Angeles Rams, San Francisco 49ers, Seattle Seahawks</li>
+            </ul>
 
-				<?php include "inc/reference.php";?>
-		</div>
-		
-	</body>	
+        </aside>
 
-</html>	
+    </section>
+
+    <?php include "inc/reference.php";?>
+
+</body>
+
+</html>
